747a0015d0b30b694b1787b5bca9e41add77b5cf
tsuyukusa-rk <rk.1227.89@gmail.com>
2016-05-02 17:03:47 +0900

    メソッドについて終わった 次はクラス

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # study_ruby
 rubyの勉強用
+
+### メモ
+・ブロックはクロージャ、メソッドはクロージャではない
+
+### 参考
+・ブロックとメソッドについての解説
+http://qiita.com/kidach1/items/15cfee9ec66804c3afd2
--- learningRuby/method.rb
+++ learningRuby/method.rb
@@ -49,3 +49,102 @@ puts prod(1,2)
 puts quo(1,2)
 
 #return
+def fact(n)
+    # nが0の場合は打ち切って、1を返す
+    return 1 if n == 1
+    product = 1
+    (1..n).each do |i|
+        product *= i
+    end
+    return product
+end
+p "return"
+p fact(0)
+p fact(3)
+def returnArray(a)
+    [a, 1, 2, 3]
+end
+p returnArray(2)
+
+#引数のデフォルト値
+# Cは省略可。
+def defaultMethod(a, b, c = 1)
+    return c
+end
+def print_time(time = Time.now)
+    p time
+end
+print_time
+sleep 1
+print_time
+
+#可変長引数
+def stretch_val(a, b, *c)
+    p [a, b, c]
+end
+stretch_val(1, 2, 3, 4, 5)
+
+#ブロック付きメソッド
+verse = "sample\nsample\n"
+count = 0
+verse.each_line do |line|
+    print line
+    count += 1
+end
+def foo_bar_baz
+    yield "foo"
+    yield "bar"
+    yield "baz"
+end
+foo_bar_baz do |item|
+    puts item
+end
+
+def foo_bar_baz3
+    # ブロックを与えられていない時、enum_forでEnumeratorを生成して返す
+    # block_given?は現在のメソッド呼び出しに対するブロックの有無をreturn
+    return enum_for(:foo_bar_baz3) unless block_given?
+    %w[foo bar baz].each do |item|
+        yield item
+    end
+end
+
+def my_map
+    [yield(1), yield(2), yield(3)]
+end
+p my_map{|i| i+1}
+
+#クロージャ
+
+#環境の保存
+#ライブラリの記述などにも用いられる
+#Proc.new
+def create_counter()
+    count = 1
+    return Proc.new do
+        count += 1
+        p count
+    end
+end
+counter = create_counter()
+p counter.class
+counter.call
+counter.call
+counter2 = create_counter()
+counter2.call
+
+#Proc
+#Procオブジェクトは、コードブロックをオブジェクトとしてみた時の名前
+class SleepyPerson
+    def register_handler(&handler)
+        @event_handler = handler
+    end
+    def wake_up!
+        @event_handler.call Time.now, "woke up"
+    end
+end
+john = SleepyPerson.new
+john.register_handler{|time, message| p [time, message]}
+john.wake_up!
+proc = Proc.new{puts "Proc was called"}
+3.times(&proc)
