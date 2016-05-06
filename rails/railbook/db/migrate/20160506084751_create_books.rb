f7f199d12b149a451996dcf3c75c7f6384686b8c
tsuyukusa-rk <rk.1227.89@gmail.com>
2016-05-06 18:03:29 +0900

    テーブルのサンプルを生成するのはできたので、次はそれを出力してみる

---
+++ rails/railbook/db/migrate/20160506084751_create_books.rb
@@ -0,0 +1,17 @@
+class CreateBooks < ActiveRecord::Migration
+  def change
+    create_table :books do |t|
+      t.string :isbn
+      t.string :title
+      t.integer :price
+      t.string :publish
+      t.date :published
+      t.boolean :cd
+
+      t.timestamps null: false
+    end
+  end
+end
+
+# マイグレーションファイルの実行
+# rake db:migrate
