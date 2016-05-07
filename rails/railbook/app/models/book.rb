dd196af1e7d4b57ffcfcb14a5e7561707fe989dc
tsuyukusa-rk <rk.1227.89@gmail.com>
2016-05-07 22:36:48 +0900

    dbを破棄するには、rake db:drop

--- rails/railbook/app/models/book.rb
+++ rails/railbook/app/models/book.rb
@@ -1,5 +1,5 @@
 # スキャッフォールド機能はそれ自体のコメンド
-# ails generate scaffold book isbn:string title:string price:integer publish:string published:date cd:boolean
+# rails generate scaffold book isbn:string title:string price:integer publish:string published:date cd:boolean
 # の他にマイグレーションは実行する必要がある
 # rake db:migrate
 class Book < ActiveRecord::Base
--- rails/railbook/db/schema.rb
+++ rails/railbook/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160506084751) do
+ActiveRecord::Schema.define(version: 20160507132637) do
 
   create_table "books", force: :cascade do |t|
     t.string   "isbn"
