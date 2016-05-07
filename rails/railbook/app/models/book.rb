503d00141af35e20c368eff07975f564115adbb9
tsuyukusa-rk <rk.1227.89@gmail.com>
2016-05-07 22:32:19 +0900

    コメントを追記

--- rails/railbook/app/models/book.rb
+++ rails/railbook/app/models/book.rb
@@ -1,2 +1,6 @@
+# スキャッフォールド機能はそれ自体のコメンド
+# ails generate scaffold book isbn:string title:string price:integer publish:string published:date cd:boolean
+# の他にマイグレーションは実行する必要がある
+# rake db:migrate
 class Book < ActiveRecord::Base
 end
